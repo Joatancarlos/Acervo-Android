6838baad1495671f802ad8f39ad98a117311656e
Joatan Carlos Farias Feitosa <joatancarlos@gmail.com>
2024-04-11 13:45:01 -0300

    Adicionando novos projetos

--- basic-android-kotlin-compose-training-mars-photos-starter/app/src/main/java/com/example/marsphotos/network/MarsApi.kt
+++
@@ -1,7 +0,0 @@
-package com.example.marsphotos.network
-
-object MarsApi {
-    val retrofitService : MarsApiServiceInteface by lazy {
-        retrofit.create(MarsApiServiceInteface::class.java)
-    }
-}
--- basic-android-kotlin-compose-training-mars-photos-starter/app/src/main/java/com/example/marsphotos/network/MarsApiServiceInteface.kt
+++
@@ -1,8 +0,0 @@
-package com.example.marsphotos.network
-
-import retrofit2.http.GET
-
-interface MarsApiServiceInteface {
-    @GET("photos")
-    suspend fun getPhotos(): String
-}
