75d6a07b90a7571b354d4fe249c1a2a921e11359
Joatan Carlos Farias Feitosa <joatancarlos@gmail.com>
2024-04-20 14:32:08 -0300

    update new project Amphibians

--- basic-android-kotlin-compose-training-mars-photos-coil-starter/app/src/main/java/com/example/marsphotos/data/MarsPhotosRepository.kt
+++ basic-android-kotlin-compose-training-mars-photos-coil-starter/app/src/main/java/com/example/marsphotos/data/MarsPhotosRepository.kt
@@ -21,17 +21,30 @@ import com.example.marsphotos.network.MarsApiService
 /**
  * Repository that fetch mars photos list from marsApi.
  */
+//interface MarsPhotosRepository {
+//    /** Fetches list of MarsPhoto from marsApi */
+//    suspend fun getMarsPhotos(): List<MarsPhoto>
+//}
+//
+///**
+// * Network Implementation of Repository that fetch mars photos list from marsApi.
+// */
+//class NetworkMarsPhotosRepository(
+//    private val marsApiService: MarsApiService
+//) : MarsPhotosRepository {
+//    /** Fetches list of MarsPhoto from marsApi*/
+//    override suspend fun getMarsPhotos(): List<MarsPhoto> = marsApiService.getPhotos()
+//}
+
 interface MarsPhotosRepository {
-    /** Fetches list of MarsPhoto from marsApi */
     suspend fun getMarsPhotos(): List<MarsPhoto>
 }
 
-/**
- * Network Implementation of Repository that fetch mars photos list from marsApi.
- */
 class NetworkMarsPhotosRepository(
     private val marsApiService: MarsApiService
 ) : MarsPhotosRepository {
-    /** Fetches list of MarsPhoto from marsApi*/
-    override suspend fun getMarsPhotos(): List<MarsPhoto> = marsApiService.getPhotos()
+    override suspend fun getMarsPhotos(): List<MarsPhoto> {
+        return marsApiService.getPhotos()
+    }
+
 }
